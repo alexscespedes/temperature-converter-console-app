9bac854c9bb6bf271ea1b48d9e6f77ae62c0ce51
alexscespedes <alexandersenc99@gmail.com>
2025-04-10 19:46:30 -0400

    Add welcome message

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Temperature Converter Console App console that converts temperatures between Fahrenheit and Celsius");
+
         }
     }
 }
