bed308d1f6ea36a6b541f95b8cf031259a9260ee
alexscespedes <alexandersenc99@gmail.com>
2025-04-11 18:26:59 -0400

    Add prompt to the user with switch

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Temperature Converter Console App console that converts temperatures between Fahrenheit and Celsius");
+            bool exit = false;
+            while(!exit) 
+            {
+                Console.WriteLine("Welcome to the Temperature Converter Console App console that converts temperatures between Fahrenheit and Celsius");
+                Console.WriteLine("1 for Celsius to Fahrenheit");
+                Console.WriteLine("2 for Fahrenheit to Celsius");
+                Console.WriteLine("3 for Exit the Program");
+                Console.Write("Choose a conversion direction: ");
+                string choice = Console.ReadLine();
+
+                switch(choice) 
+                {
+                    case "1":
+                        Console.WriteLine("Hello 1");
+                        break;
+                    case "2":
+                        Console.WriteLine("Hello 2");
+                        break;
+                    case "3":
+                        Console.WriteLine("Exiting the program...");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Try again!");
+                        break;
+                }
+            }
 
         }
     }
